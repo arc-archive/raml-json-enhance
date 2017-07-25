1e07d3a669783bd704ed4a90a3b2ff1cf9c122df
Pawel Psztyc <jarrodek@gmail.com>
2017-07-26 00:05:25 +0100

    Added more optimization to reduce build size.

--- build-element.sh
+++ build-element.sh
@@ -1,8 +1,11 @@
-./node_modules/.bin/browserify node_modules/raml2obj/browser.js -o build/browserified.js --ignore datatype-expansion
-./node_modules/.bin/babel build/browserified.js > build/babeled.js
-./node_modules/.bin/uglifyjs -c --screw-ie8 build/babeled.js > build/raml2object.js
-./node_modules/.bin/uglifyjs -c --screw-ie8 lib/polyfills.js > build/polyfills.js
-./node_modules/.bin/uglifyjs -c --screw-ie8 bower_components/promise-polyfill/Promise.js > build/Promise.js
-./node_modules/.bin/uglifyjs -c --screw-ie8 bower_components/promise-polyfill/Promise-Statics.js > build/Promise-Statics.js
-./node_modules/.bin/uglifyjs -c --screw-ie8 lib/normalize-worker.js > build/normalize-worker.js
+export PATH=./node_modules/.bin:$PATH
+
+browserify node_modules/raml2obj/browser.js -o build/browserified.js --ignore datatype-expansion
+babel build/browserified.js > build/babeled.js
+uglifyjs -c --screw-ie8 build/babeled.js > build/raml2object.js
+uglifyjs -c --screw-ie8 lib/polyfills.js > build/polyfills.js
+uglifyjs -c --screw-ie8 bower_components/promise-polyfill/Promise.js > build/Promise.js
+uglifyjs -c --screw-ie8 bower_components/promise-polyfill/Promise-Statics.js > build/Promise-Statics.js
+babel --plugins minify-mangle-names lib/normalize-worker.js > build/normalize-worker-babeled.js
+uglifyjs -c --screw-ie8 build/normalize-worker-babeled.js > build/normalize-worker.js
 node build-element.js
