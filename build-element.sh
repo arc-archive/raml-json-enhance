2b4d2a1b015010e7fbe25c92dc3af93ed68d83b1
Paweł Psztyć <jarrodek@gmail.com>
2017-12-05 16:41:47 -0800

    Feature/datatype upgrade (#3)
    * Update: Updated version of the datatype library. Simplified workflow.
    
    * Update: Updated version of datatype expansion library.
    
    * Update: Updates tests configuration
    
    * Fix: Restored Promise polyfill
    
    * Fix: Restoring polyfills for IE11
    
    * Fix: Added polyfills for expansion library.

--- build-element.sh
+++ build-element.sh
@@ -5,22 +5,11 @@ function ugly() {
   echo  `uglifyjs -c --screw-ie8 $1`
 }
 
-function compileExpander() {
-  local basePath=`pwd`
-  projectClj=`cat lib/project.clj`
-  cd ./node_modules/datatype-expansion/
-  # temporary fix until next expansion library release.
-  echo ${projectClj} > project.clj
-  rm -rf browser/*
-  lein cljsbuild once browser
-  cp -R browser "${basePath}/build/"
-  cd $basePath
-}
-
 function compileRaml2obj() {
   browserify node_modules/raml2obj/browser.js -o build/browserified.js --ignore datatype-expansion
   babel build/browserified.js > build/babeled.js
   ugly "build/babeled.js"
+  # cat build/babeled.js
 }
 
 function compileWorker() {
@@ -30,24 +19,27 @@ function compileWorker() {
 
 # First build the expnasion library, browser version.
 rm -rf build/*
-compileExpander || exit 1
 
 contents=()
 contents[0]=$(ugly "lib/polyfills.js")
 contents[1]=$(ugly "bower_components/promise-polyfill/Promise.js")
-contents[2]=`cat build/browser/index.js`
+contents[2]=`cat ./node_modules/datatype-expansion/dist/datatype-expansion.js`
 contents[3]=$(compileRaml2obj)
 contents[4]=$(ugly "lib/init-script.js")
 contents[5]=$(ugly "bower_components/promise-polyfill/Promise-Statics.js")
 contents[6]=$(compileWorker)
 
-# Concatenates all the files. Adds the ";" between each file.
 content=""
 for data in "${contents[@]}"
 do
-  content="${content};${data}"
+  content=$(printf "%s\n%s" "${content}" "${data}")
 done;
+
+# content="${contents[0]contents[1]}"
+
+# echo ${content:0:300}
 
 # Saves template data into the final build
 template=`cat raml-json-enhance-template.html`
 echo "${template/\{\{RAML2OBJ_CONTENT\}\}/$content}" > ./raml-json-enhance.html
+echo "Build complete."
